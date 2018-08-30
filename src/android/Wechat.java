8acbecc5c705203da08550f667702887d95eb166
袁家强 <1406736431@qq.com>
2018-08-30 16:06:13 +0800

    Update Wechat.java

--- src/android/Wechat.java
+++ src/android/Wechat.java
@@ -595,7 +595,7 @@ public class Wechat extends CordovaPlugin {
         return null;
     }
 
-    public static String getAppId() {
+    public String getAppId() {
         if (appId == null) {
             appId = preferences.getString(WXAPPID_PROPERTY_KEY, "");
         }
@@ -608,7 +608,7 @@ public class Wechat extends CordovaPlugin {
      * @param ctx
      * @return
      */
-    public static String getSavedAppId(Context ctx) {
+    public String getSavedAppId(Context ctx) {
         SharedPreferences settings = ctx.getSharedPreferences(PREFS_NAME, 0);
         return settings.getString(WXAPPID_PROPERTY_KEY, "");
     }
@@ -618,7 +618,7 @@ public class Wechat extends CordovaPlugin {
      * @param ctx
      * @param id
      */
-    public static void saveAppId(Context ctx, String id) {
+    public void saveAppId(Context ctx, String id) {
         if (id.isEmpty()) {
             return ;
         }

615c63135167e5ff8c35e068179f245e846d6910
袁家强 <1406736431@qq.com>
2018-09-03 10:00:57 +0800

    Update Wechat.java

--- src/android/Wechat.java
+++ src/android/Wechat.java
@@ -597,7 +597,7 @@ public class Wechat extends CordovaPlugin {
 
     public String getAppId() {
         if (appId == null) {
-            appId = "wxdffdb35a8cc3a6cd";//preferences.getString(WXAPPID_PROPERTY_KEY, "");
+            appId = preferences.getString(WXAPPID_PROPERTY_KEY, "");
         }
 
         return appId;
